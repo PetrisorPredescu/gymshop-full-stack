a62ac1ad614f2011bad5e2d718c426d4d80ceb78
PetrisorPredescu <79548847+PetrisorPredescu@users.noreply.github.com>
2022-06-10 00:03:34 +0300

    tweaks

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,190 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const { Double } = require('mongodb');
+
+const mongoose = require('mongoose');
+const db = require('../config/keys').mongoURI;
+var ObjectId = require('mongodb').ObjectID;
+
 
 // Welcome Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+router.get('/', forwardAuthenticated, (req, res) => res.render('dashboard'));
 
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
+router.get('/dashboard', (req, res) => //, ensureAuthenticated
   res.render('dashboard', {
     user: req.user
   })
 );
+// Contact page
+router.get('/contact', (req, res) => //, ensureAuthenticated
+  res.render('contact', {
+    user: req.user
+  })
+);
+// products page
+router.get('/products', (req, res) => //, ensureAuthenticated
 
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    if (err) { console.log(err) } else {
+      db.collection("products").find().sort({ "price":1}).toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('products', {
+            user: req.user,
+            products: result
+          })
+        }
+      })
+    }
+  }))
+
+// products submit page
+router.post('/products', (req, res) => //, ensureAuthenticated
+
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    var { name, qty, customer, address, email, cardnumber, expirydate, cvv } = req.body;
+    if (err) { console.log(err) } else {
+
+      db.collection("orders").insertOne({
+        "name": name,
+        "qty": qty,
+        "customer": customer,
+        "address": address,
+        "email": email,
+        "cardnumber": cardnumber,
+        "expirydate": expirydate,
+        "cvv": cvv
+      });
+
+      db.collection("products").find().sort({ "price":1}).toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('products', {
+            user: req.user,
+            products: result,
+            success_msg: "Thank you for your order. It will be shipped in a couple of days."
+          })
+        }
+      })
+    }
+  })
+)
+
+
+
+// Contact post page
+router.post('/contact', (req, res) =>
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    var { name, email, message } = req.body;
+    if (err) { console.log(err) } else {
+      db.collection("contacts").insertOne({
+        "name": name,
+        "email": email,
+        "message": message
+      });
+      res.render('contact', {
+        user: req.user,
+        success_msg: "Your message has been successfully sent! Thank you!"
+      })
+
+    }
+  }))
+
+
+// product manager page
+router.get('/product_manager', (req, res) => //, ensureAuthenticated
+
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    if (err) { console.log(err) } else {
+      db.collection("products").find().sort({ "price":1}).toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('product_manager', {
+            user: req.user,
+            products: result
+          })
+        }
+      })
+    }
+  }))
+
+// post products manager page update
+router.post('/product_manager1', (req, res) => //, ensureAuthenticated
+
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    var { product_id, name, price, photo } = req.body;
+    if (err) { console.log(err) } else {
+      delete req.body._id;
+      db.collection("products").updateOne({ _id: new ObjectId(product_id) }, { $set: { "price": price, "name": name, "photo": photo } });  // yes i know its deprecated!!! but still works! :D
+
+      db.collection("products").find().sort({ "price":1}).toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('product_manager', {
+            user: req.user,
+            products: result,
+            success_msg: "Product updated!"
+          })
+        }
+      })
+    }
+  })
+)
+// post products manager page insert
+router.post('/product_manager2', (req, res) => //, ensureAuthenticated
+
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    var { product_id, name, price, photo } = req.body;
+    if (err) { console.log(err) } else {
+
+      db.collection("products").insertOne({ "price": Double(price), "name": name,"photo": photo } );  
+
+      db.collection("products").find().sort({ "price":1}).toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('product_manager', {
+            user: req.user,
+            products: result,
+            success_msg: "Product updated!"
+          })
+        }
+      })
+    }
+  })
+)
+// post products manager page delete
+router.post('/product_manager3', (req, res) => //, ensureAuthenticated
+
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    var { product_id, name, price } = req.body;
+    if (err) { console.log(err) } else {
+
+      db.collection("products").deleteOne({ _id: new ObjectId(product_id) });  
+
+      db.collection("products").find().sort({ "price":1}).toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('product_manager', {
+            user: req.user,
+            products: result,
+            success_msg: "Product updated!"
+          })
+        }
+      })
+    }
+  })
+)
+
+// products page
+router.get('/orders_manager', (req, res) => //, ensureAuthenticated
+
+  mongoose.createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    if (err) { console.log(err) } else {
+      db.collection("orders").find().toArray(function (err, result) {
+        if (err) { console.log(err) } else {
+          res.render('orders_manager', {
+            user: req.user,
+            orders: result
+          })
+        }
+          
+      })
+    }
+  }))
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,7 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/dashboard',
-    failureRedirect: '/users/login',
+    failureRedirect: '/dashboard',
     failureFlash: true
   })(req, res, next);
 });
@@ -90,7 +90,7 @@ router.post('/login', (req, res, next) => {
 router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  res.redirect('/dashboard');
 });
 
 module.exports = router;
